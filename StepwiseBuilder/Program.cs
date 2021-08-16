5dadb96256f9c5393c915de5933ec69944f5b18d
Jabrayilzade <cnixad@gamil.com>
2021-08-16 11:12:41 +0400

    Stepwise builder

---
+++ StepwiseBuilder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StepwiseBuilder
+{
+    public enum CarType
+    {
+        Sedan,
+        Crossover
+    }
+
+    public class Car
+    {
+        public CarType Type;
+        public int WheelSize;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
